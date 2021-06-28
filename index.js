db3d7c8ff0ccacebfe33181f3ddb2b106dcb26e5
Matt Fleming <36967171+mflem8@users.noreply.github.com>
2021-06-28 11:26:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function fetchBooks() {
-  return fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-  .then(json => renderBooks(json)); 
   
-}
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(json => renderBooks(json));
+
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
